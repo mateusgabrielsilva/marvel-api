f186e911e8db8a4ceedf1eca7df5f0d3ad187821
mateusgabrielsilva <mateus92.mgs@gmail.com>
2022-09-21 22:25:22 -0300

    inicando aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+// import * as md5 from 'module-name';
+const md5 = require('md5');
+
+
+const publicKey = 'b8570335876a14504dccb85cac52d47d'
+const privateKey = '4bd704c4fddd35d633d1acfd3f1fe76e5884ed1b'
+
+const time = Number(new Date())
+
+const hash = md5(time + privateKey + publicKey)
+
+const App: React.FC = () => {
+  useEffect(() => {
+    axios
+    .get(`http://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${publicKey}&hash=${hash}`,)
+    .then(response => console.log(time, response.data.data.results))
+    .catch(err => console.log(err))
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Marvel
     </div>
   );
-}
+};
 
 export default App;
